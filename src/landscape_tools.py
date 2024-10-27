3ae8275dd034c2e31170a6da5833f62249e69e52
st-sch <stevenschulz@live.fr>
2024-10-27 13:46:26 +0100

    finished code for Fig. 1C

--- src/landscape_tools.py
+++ src/landscape_tools.py
@@ -1,4 +1,5 @@
 import numpy as np, src.sparray as sp
+from scipy.sparse import dok_matrix
 
 
 def seqlist(q=2, L=1):
@@ -27,6 +28,34 @@ class EmpLS:
     def fitness(self, seq=(0)):
         return self.fmat[seq]
 
+def locmax(q, L, fitness, avseqs=[], nrmax=np.inf):
+    # find local maxima of a landscape in sequence space
+        
+    fmat = np.nan*np.ones([q]*L, dtype='float')
+    if len(avseqs) == 0:
+        for seq in seqlist(q=q, L=L):
+            fmat[seq] = fitness(seq=seq)
+    else:
+        for seq in avseqs:
+            fmat[seq] = fitness(seq=seq)
+        fmat[np.isnan(fmat)] = -np.inf
+
+    # ls_dx:= dummy copies of the landscape, maxs:= dict with positions and values of local maxima in the landscape
+    fmat_d1, fmat_d2, smax = fmat.copy(), fmat.copy(), {}
+
+    while np.max(fmat_d1) > -np.inf and len(smax) < nrmax:
+        # find position of maximal remaining fitness in landscape, and add it to dict
+        inds = np.unravel_index(np.argmax(fmat_d2, axis=None), fmat.shape)
+        if fmat_d1[inds] > -np.inf:
+            smax[inds] = fmat[inds]
+        # set local maximum to minus infinity
+        fmat_d2[inds] = -np.inf
+        # set all neighbors of that maximum (seqs accessible within one mutation) to minus infinity (as they cannot be local maxima)
+        for i in range(L):
+            fmat_d1[inds[:i]+(slice(fmat.shape[i]),)+inds[i+1:]] = -np.inf
+
+    return smax
+
 def mutate(seq, sites):
     # flip the state of the specified 'sites' in binary sequence 'seq'
     seq = list(seq)
@@ -48,3 +77,134 @@ def gammaij(L, seqs, fitness):
                 denom += (fitness(mutate(s, [j])) - fitness(s))**2
             gamma[i,j] = nom/denom
     return gamma
+
+def mkM(q=2, L=10, order=2, ising=False, seqs=[]):
+    # computes matrix M that maps vector of model parameters X to vector
+    # of fitness values F: F=M.X
+    if len(seqs) == 0:
+        seqs = seqlist(q=q, L=L)
+    
+    if order == 1:
+        M = dok_matrix((len(seqs), L), dtype=int)
+        for x, seq in enumerate(seqs):
+            for i in range(L):
+                if ising:
+                    M[x,i] = 2*seq[i]-1
+                else:
+                    if seq[i]==1:
+                        M[x,i] = 1
+    
+    elif order == 2:
+        M = dok_matrix((len(seqs), L+int(L*(L-1)/2)), dtype=int)
+        for x, seq in enumerate(seqs):
+            jcnt = 0
+            for i in range(L):
+                if ising:
+                    M[x,i] = 2*seq[i]-1
+                else:
+                    if seq[i]==1:
+                        M[x,i] = 1
+                for j in range(i):
+                    if ising:
+                        M[x,L+jcnt] = (2*seq[i]-1)*(2*seq[j]-1)
+                    else:
+                        if seq[i]==1 and seq[j]==1:
+                            M[x,L+jcnt] = 1
+                    jcnt += 1
+    
+    elif order == 3:
+        M = dok_matrix((len(seqs), L+int(L*(L-1)/2)+int(L*(L-1)*(L-2)/(3*2))), dtype=int)
+        for x, seq in enumerate(seqs):
+            jcnt, kcnt = 0, 0
+            for i in range(L):
+                if ising:
+                    M[x,i] = 2*seq[i]-1
+                else:
+                    if seq[i]==1:
+                        M[x,i] = 1
+                for j in range(i):
+                    if ising:
+                        M[x,L+jcnt] = (2*seq[i]-1)*(2*seq[j]-1)
+                    else:
+                        if seq[i]==1 and seq[j]==1:
+                            M[x,L+jcnt] = 1
+                    jcnt += 1
+                    for k in range(j):
+                        if ising:
+                            M[x,L+int(L*(L-1)/2)+kcnt] = (2*seq[i]-1)*(2*seq[j]-1)*(2*seq[k]-1)
+                        else:
+                            if seq[i]==1 and seq[j]==1 and seq[k]==1:
+                                M[x,L+int(L*(L-1)/2)+kcnt] = 1
+                        kcnt += 1
+    
+    elif order == 4:
+        M = dok_matrix((len(seqs), L+int(L*(L-1)/2)+int(L*(L-1)*(L-2)/(3*2))+int(L*(L-1)*(L-2)*(L-3)/(4*3*2))), dtype=int)
+        for x, seq in enumerate(seqs):
+            jcnt, kcnt, hcnt = 0, 0, 0
+            for i in range(L):
+                if ising:
+                    M[x,i] = 2*seq[i]-1
+                else:
+                    if seq[i]==1:
+                        M[x,i] = 1
+                for j in range(i):
+                    if ising:
+                        M[x,L+jcnt] = (2*seq[i]-1)*(2*seq[j]-1)
+                    else:
+                        if seq[i]==1 and seq[j]==1:
+                            M[x,L+jcnt] = 1
+                    jcnt += 1
+                    for k in range(j):
+                        if ising:
+                            M[x,L+int(L*(L-1)/2)+kcnt] = (2*seq[i]-1)*(2*seq[j]-1)*(2*seq[k]-1)
+                        else:
+                            if seq[i]==1 and seq[j]==1 and seq[k]==1:
+                                M[x,L+int(L*(L-1)/2)+kcnt] = 1
+                        kcnt += 1
+                        for h in range(k):
+                            if ising:
+                                M[x,L+int(L*(L-1)/2)+int(L*(L-1)*(L-2)/(3*2))+hcnt] = (2*seq[i]-1)*(2*seq[j]-1)*(2*seq[k]-1)*(2*seq[h]-1)
+                            else:
+                                if seq[i]==1 and seq[j]==1 and seq[k]==1 and seq[h]==1:
+                                    M[x,L+int(L*(L-1)/2)+int(L*(L-1)*(L-2)/(3*2))+hcnt] = 1
+                            hcnt += 1
+    
+    elif order == 5:
+        M = dok_matrix((len(seqs), L+int(L*(L-1)/2)+int(L*(L-1)*(L-2)/(3*2))+int(L*(L-1)*(L-2)*(L-3)/(4*3*2))+int(L*(L-1)*(L-2)*(L-3)*(L-4)/(5*4*3*2))), dtype=int)
+        for x, seq in enumerate(seqs):
+            jcnt, kcnt, hcnt, gcnt = 0, 0, 0, 0
+            for i in range(L):
+                if ising:
+                    M[x,i] = 2*seq[i]-1
+                else:
+                    if seq[i]==1:
+                        M[x,i] = 1
+                for j in range(i):
+                    if ising:
+                        M[x,L+jcnt] = (2*seq[i]-1)*(2*seq[j]-1)
+                    else:
+                        if seq[i]==1 and seq[j]==1:
+                            M[x,L+jcnt] = 1
+                    jcnt += 1
+                    for k in range(j):
+                        if ising:
+                            M[x,L+int(L*(L-1)/2)+kcnt] = (2*seq[i]-1)*(2*seq[j]-1)*(2*seq[k]-1)
+                        else:
+                            if seq[i]==1 and seq[j]==1 and seq[k]==1:
+                                M[x,L+int(L*(L-1)/2)+kcnt] = 1
+                        kcnt += 1
+                        for h in range(k):
+                            if ising:
+                                M[x,L+int(L*(L-1)/2)+int(L*(L-1)*(L-2)/(3*2))+hcnt] = (2*seq[i]-1)*(2*seq[j]-1)*(2*seq[k]-1)*(2*seq[h]-1)
+                            else:
+                                if seq[i]==1 and seq[j]==1 and seq[k]==1 and seq[h]==1:
+                                    M[x,L+int(L*(L-1)/2)+int(L*(L-1)*(L-2)/(3*2))+hcnt] = 1
+                            hcnt += 1
+                            for g in range(h):
+                                if ising:
+                                    M[x,L+int(L*(L-1)/2)+int(L*(L-1)*(L-2)/(3*2))+int(L*(L-1)*(L-2)*(L-3)/(4*3*2))+gcnt] = (2*seq[i]-1)*(2*seq[j]-1)*(2*seq[k]-1)*(2*seq[h]-1)*(2*seq[g]-1)
+                                else:
+                                    if seq[i]==1 and seq[j]==1 and seq[k]==1 and seq[h]==1 and seq[g]==1:
+                                        M[x,L+int(L*(L-1)/2)+int(L*(L-1)*(L-2)/(3*2))+int(L*(L-1)*(L-2)*(L-3)/(4*3*2))+gcnt] = 1
+                                gcnt += 1
+    return M
